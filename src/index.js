eb80ba0f505940c21ff5822ab11d78cc1767c0c1
pedrojaokkj <jpdr3551@gmail.com>
2024-10-25 01:40:45 -0300

    Aula 10 - View Engines

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const rotaUsuario = require('./rotas/usuario.rota')
 const rotaPost = require('./rotas/post.rota')
+var expressLayouts = require('express-ejs-layouts')
 
 const app = express()
 app.use(express.json())
+app.set('view engine', 'ejs')
+
+app.set('layout', 'layouts/layout')
+
+app.use(expressLayouts)
 
 app.use('/static', express.static('public'))
 
@@ -15,6 +21,32 @@ app.get('/', (req, res) => {
     res.send('Hello World from Express')
 })
 
+//get página
+app.get('/home', (req, res) => {
+    const number = Math.random()
+    res.render('pages/index', {variavel : number})
+})
+
+//get página cursos
+app.get('/cursos', (req, res) => {
+    const cursos = [
+            {nome: "Programação fronend", ch: 280}, 
+            {nome: "Programação backend", ch: 330},
+            {nome: "Programação concorrente", ch: 300},
+            {nome: "Programação distribuída", ch: 400}
+    ]
+    res.render('pages/cursos/index', {cursos: cursos})
+})
+
+
+app.get('/alunos', (req, res) => {
+    const alunos = [
+            {nome: "João Pedro"},
+            {nome: "Fernanda"},
+            {nome: "Francisco"}
+    ]
+    res.render('pages/alunos/index', {alunos: alunos})
+})
 
 
 //servidor
